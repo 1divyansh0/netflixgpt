9480c19af7c149bb69f5d20e3574874850c66226
1divyansh0 <rajvishwa1983@gmail.com>
2025-06-21 20:54:18 +0530

    security

--- Components/gptsearch.jsx
+++ Components/gptsearch.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenerativeAI } from '@google/generative-ai';
 import options from '../Constant/Cons'
 import Movielist from './movielist';
 import { useDispatch, useSelector } from 'react-redux';
@@ -11,20 +11,21 @@ const gptsearch = () => {
   
   const searchdata = useRef(null);
   // const [searchedmovie, setsearchedmovie] = useState("");
+   const apikey = import.meta.env.VITE_API_KEY;
    const dispatch = useDispatch();
    const {gptmoviesname,gptmovielist} = useSelector(state=>state.gpt);
    
   
 
-  const ai = new GoogleGenAI({ apiKey: "AIzaSyD_u7a0WVZG9fJJpwspzB6jblMaqcc23yo" });
+  const ai = new GoogleGenerativeAI(apikey);
  
 async function main(prompt) {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: prompt,
-  });
-  // console.log(response.text)
-  return response.text;
+ const model = ai.getGenerativeModel({ model: "gemini-1.5-flash" }); // or "gemini-pro", "gemini-1.0-pro", etc.
+
+    const result = await model.generateContent(prompt);
+    const text =  result.response.text();
+ console.log(text)
+  return text;
 }
 //  console.log(searchedmovie);
 
